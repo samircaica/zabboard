663a3f7caee473e1b60233141ff9ae09eafce0a9
Samir Caica <samir.caica@gmail.com>
2015-09-01 11:35:29 -0300

    too many redirect fix

--- public/index.php
+++ public/index.php
@@ -17,9 +17,9 @@ if(!isset($_SESSION['params'])) {
 /*
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)));
- 
+*/ 
 $url = $_GET['url'];
-*/
+
 //require_once (ROOT . DS . 'library' . DS . 'bootstrap.php');
 require_once (ROOT . DS . 'library' . DS . 'Base.php');
 
